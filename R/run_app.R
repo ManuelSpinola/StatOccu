2046cbf66a01fba9c4cde0efde0ac0bec4523b83
Manuel Spínola <mspinola10@gmail.com>
2026-05-19 11:24:27 -0600

    fix: import bslib shiny en run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -1,23 +1,30 @@
-#' Run the StatOccu Shiny Application
+#' Run the Shiny Application
 #'
-#' @description Launches the StatOccu interactive occupancy modeling
-#'   platform in your default web browser.
-#'
-#' @param ... Arguments passed to \code{\link[golem]{with_golem_options}}.
-#'
-#' @return No return value, called for side effects.
-#'
-#' @examples
-#' \dontrun{
-#'   StatOccu::run_app()
-#' }
+#' @param ... arguments to pass to golem_opts.
+#' @inheritParams shiny::shinyApp
 #'
 #' @export
-run_app <- function(...) {
-  golem::with_golem_options(
-    app = shiny::shinyApp(
-      ui     = app_ui(),
-      server = app_server
+#' @importFrom shiny shinyApp
+#' @importFrom golem with_golem_options
+#' @import bslib
+#' @import shiny
+#' @import bsicons
+#' @import shinyjs
+run_app <- function(
+  onStart = NULL,
+  options = list(),
+  enableBookmarking = NULL,
+  uiPattern = "/",
+  ...
+) {
+  with_golem_options(
+    app = shinyApp(
+      ui     = app_ui,
+      server = app_server,
+      onStart = onStart,
+      options = options,
+      enableBookmarking = enableBookmarking,
+      uiPattern = uiPattern
     ),
     golem_opts = list(...)
   )
